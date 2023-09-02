d29025f0eb606ccab5e5fe00d9631f1c7398811f
Chan Dinh <dinhhoangbaochan@gmail.com>
2023-09-02 14:18:12 +0700

    setup city search component

---
+++ src/components/SearchCity.vue
@@ -0,0 +1,25 @@
+<template>
+  <input type="text" @keyup="findCity($event)" @input="checkInput">
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const citySearch = ref('');
+const debounce = ref(null);
+
+const checkInput = () => {
+  clearTimeout(debounce.value);
+  debounce.value = setTimeout(() => {
+    getCity();
+  }, 500)
+}
+
+const findCity = (event) => {
+  citySearch.value = event.target.value;
+}
+
+const getCity = () => {
+  
+}
+</script>
