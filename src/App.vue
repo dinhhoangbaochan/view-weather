6741f953b3e3b3803fa8d38b7a204fb549eb7220
Chan Dinh <dinhhoangbaochan@gmail.com>
2023-09-02 13:42:18 +0700

    setup env

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 import AppHeader from './components/AppHeader.vue'
 import AppBody from './components/AppBody.vue';
 
-const apiKey = 'ec12ae422dbeabd6ba46c9483c46606e';
+const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY
 const cityName = 'London';
 const countryCode = '+84';
 
 // const weatherEndPoint = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&appid=${appID}`;
-const geocodingEndPoint = `http://api.openweathermap.org/geo/1.0/direct?q=Ho Chi Minh&limit=1&appid=ec12ae422dbeabd6ba46c9483c46606e`;
+const geocodingEndPoint = `http://api.openweathermap.org/geo/1.0/direct?q=Ho Chi Minh&limit=1&appid=${apiKey}`;
 
 // const getGeocoding = async () => {
 //   const req = await fetch(geocodingEndPoint, {method: 'GET'});
@@ -16,7 +16,7 @@ const geocodingEndPoint = `http://api.openweathermap.org/geo/1.0/direct?q=Ho Chi
 //   return res;
 // }
 
-// console.log(getGeocoding());
+console.log(geocodingEndPoint);
 </script>
 
 <template>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv  } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { env } from 'node:process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    WEATHER_API_KEY: env.WEATHER_API_KEY
+  },
   plugins: [
     vue(),
     vueJsx(),
