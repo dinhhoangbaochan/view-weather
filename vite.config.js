08e5a0e75468843b2ac771392946bb4422a0c2a6
Chan Dinh <dinhhoangbaochan@gmail.com>
2023-09-02 13:42:39 +0700

    revert vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv  } from 'vite'
+import { defineConfig  } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { env } from 'node:process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    WEATHER_API_KEY: env.WEATHER_API_KEY
-  },
   plugins: [
     vue(),
     vueJsx(),
